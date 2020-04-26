b168959d9abede5a9fabf997b80bbee97e9e55e5
Awhelan24 <39684894+Awhelan24@users.noreply.github.com>
2020-04-26 17:12:08 -0400

    Update NNclassifier_test.py

--- neuralNetworks/code/NNclassifier_test.py
+++ neuralNetworks/code/NNclassifier_test.py
@@ -1,1 +1,38 @@
-//placeholder
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+
+imshow(torchvision.utils.make_grid(images))
+print('GroundTrute: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+net = Net()
+net.load_state_dict(torch.load(PATH))
+
+outputs = net(images)
+
+_, predicted = torch.max(outputs, 1)
+
+print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
+
+
+correct = 0
+total = 0
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+
+with torch.no_grad():
+  for data in testloader:
+    images, labels = data
+    outputs = net(images)
+    _, predicted = torch.max(outputs.data, 1)
+    c = (predicted == labels).squeeze()
+    total += labels.size(0)
+    correct += (predicted == labels).sum().item()
+    for i in range(4):
+      label = labels[i]
+      class_correct[label] += c[i].item()
+      class_total[label] += 1
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
+
+for i in range(10):
+  print('Accuracy of %5s : %2d %%' % (classes[i], 100 * class_correct[i] / class_total[i]))
